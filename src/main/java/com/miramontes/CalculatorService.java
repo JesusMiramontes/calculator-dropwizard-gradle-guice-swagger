66b171e8317b23766ea0de95886928b8b9b05e07
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-09-02 17:04:44 -0500

    Created CalculatorService

---
+++ src/main/java/com/miramontes/CalculatorService.java
@@ -0,0 +1,41 @@
+package com.miramontes;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class CalculatorService {
+
+    public static Integer add(List<Integer> numbers){
+        return
+                numbers.stream()
+                        .filter(Objects::nonNull)
+                        .collect(
+                                Collectors.summingInt(Integer::intValue)
+                        );
+    }
+
+    public static Long subtract(List<Integer> numbers) {
+        List<Integer> filtered =
+                numbers
+                        .stream()
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toList());
+        if(filtered.isEmpty()){return 0l;}
+        Integer start = filtered.remove(0);
+        return filtered.stream().mapToLong(Integer::longValue).reduce(start, (a, b) -> a - b);
+    }
+
+    public static Long multiply(List<Integer> numbers) {
+        List<Integer> filtered = numbers.stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        if (filtered.isEmpty()) {
+            return 0l;
+        }
+        return filtered.stream()
+                .mapToLong(Integer::longValue)
+                .reduce(1, (a, b) -> a * b);
+    }
+
+}
