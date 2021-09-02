8545ed7dc4f212eefe8b71a763483ac9c3c925e0
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-09-02 17:03:34 -0500

    Created application class with guice integration

---
+++ src/main/java/com/miramontes/CalculatorApplication.java
@@ -0,0 +1,25 @@
+package com.miramontes;
+
+import config.CalculatorConfiguration;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import ru.vyarus.dropwizard.guice.GuiceBundle;
+
+public class CalculatorApplication extends Application<CalculatorConfiguration> {
+    @Override
+    public void run(CalculatorConfiguration configuration, Environment environment) throws Exception {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new CalculatorApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<CalculatorConfiguration> bootstrap){
+        bootstrap.addBundle(GuiceBundle.builder()
+                .enableAutoConfig(getClass().getPackage().getName())
+                .build());
+    }
+}
