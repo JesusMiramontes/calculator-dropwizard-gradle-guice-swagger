ce7c2fc9f1a18035efcf8c877be2be827a92c3ab
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-09-06 15:39:11 -0500

    Spotless apply

--- src/main/java/com/miramontes/CalculatorApplication.java
+++ src/main/java/com/miramontes/CalculatorApplication.java
@@ -10,20 +10,20 @@ import io.dropwizard.setup.Environment;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import io.swagger.v3.oas.integration.SwaggerConfiguration;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import ru.vyarus.dropwizard.guice.GuiceBundle;
-
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import ru.vyarus.dropwizard.guice.GuiceBundle;
 
 public class CalculatorApplication extends Application<CalculatorConfiguration> {
     @Override
     public void run(CalculatorConfiguration configuration, Environment environment)
             throws Exception {
-        environment.jersey().register(new OpenApiResource().openApiConfiguration(getOpenApiConfiguration()));
+        environment
+                .jersey()
+                .register(new OpenApiResource().openApiConfiguration(getOpenApiConfiguration()));
     }
 
     public static void main(String[] args) throws Exception {
@@ -43,9 +43,10 @@ public class CalculatorApplication extends Application<CalculatorConfiguration> 
 
     private SwaggerConfiguration getOpenApiConfiguration() {
         final OpenAPI openAPI = new OpenAPI();
-        final Info info = new Info()
-                .title("Simple calculator api")
-                .description("Using Dropwizard, gradle, google guice, swagger, open api");
+        final Info info =
+                new Info()
+                        .title("Simple calculator api")
+                        .description("Using Dropwizard, gradle, google guice, swagger, open api");
         openAPI.info(info);
         final List<Server> servers = Stream.of(new Server().url("/")).collect(Collectors.toList());
         openAPI.servers(servers);
